5f545e13e563a733b677fa51f186716d5e7bcb76
Froissart Kevin <kevin.froissart69@gmail.com>
2020-12-25 18:49:30 +0100

    boutton component

--- app/components/index.js
+++ app/components/index.js
@@ -1,2 +1,3 @@
 export { default as Screen } from './AppScreen';
 export { default as Text } from './AppText';
+export { default as Button } from './AppButton';
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import { Screen, Text } from '../components/index';
+import { Screen, Text, Button } from '../components/index';
+import colors from '../config/colors';
 
 function HomeScreen(props) {
     return (
         <Screen>
             <View style={styles.container}>
                 <Text>Open up App.js to start working on your app!</Text>
+                <Button title='Ceci est un boutton' onPress={() => console.log("Button pressed")}/>
             </View>
         </Screen>
     );
