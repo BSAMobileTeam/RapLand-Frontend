36cd3bacc0f670062f2ddbc0df886ab3f9efce28
Froissart Kevin <kevin.froissart69@gmail.com>
2020-12-27 01:31:37 +0100

    Add questions to JSON as per #7

--- app/data-temp/question.js
+++ app/data-temp/question.js
@@ -25,6 +25,83 @@ const questions = [
             "reponse": "Le Rat Luciano",
         },
     },
+    {
+        "id": 3,
+        "theme": "rap_fr",
+        "mode": "solo",
+        "type_question": "unique",
+        "question": {
+            "intitule": "Combien y a-t-il de rappeurs dans 13 Organisé",
+            "reponse": "50",
+        },
+    },
+    {
+        "id": 4,
+        "theme": "rap_fr",
+        "mode": "solo",
+        "type_question": "fermee",
+        "question": {
+            "intitule": "Quel Media Rap a sorti un album en 2020",
+            "choix": [
+                "RAPLUME", "KULTURE", "BOOSKA-P", "RAPUNCHLINE",
+            ],
+            "reponse": "RAPLUME",
+        },
+    },
+    {
+        "id": 4,
+        "theme": "rap_fr",
+        "mode": "solo",
+        "type_question": "fermee",
+        "question": {
+            "intitule": "Quel Media Rap a sorti un album en 2020",
+            "choix": [
+                "RAPLUME", "KULTURE", "BOOSKA-P", "RAPUNCHLINE",
+            ],
+            "reponse": "RAPLUME",
+        },
+    },
+    {
+        "id": 5,
+        "theme": "rap_fr",
+        "mode": "solo",
+        "type_question": "fermee",
+        "question": {
+            "intitule": "Quel rappeur est resté le plus longtemps du top 1 Spotify",
+            "choix": [
+                "Jul", "SCH", "Ninho", "Booba",
+            ],
+            "reponse": "SCH",
+        },
+    },
+    {
+        "id": 6,
+        "theme": "rap_fr",
+        "mode": "solo",
+        "type_question": "fermee",
+        "question": {
+            "intitule": "Avec qui Alkapote n'a-t-il pas fait de feat",
+            "choix": [
+                "Bilal Hassani", "Freeze Corleone", "Kaaris", "Kalash",
+            ],
+            "reponse": "Kalash",
+        },
+    },
+    {
+        "id": 7,
+        "theme": "rap_fr",
+        "mode": "solo",
+        "type_question": "multiple",
+        "question": {
+            "intitule": "Lesquels des ses rappeurs sont apparu dans un film",
+            "choix": [
+                "Kaaris", "Fianso", "Jok'Air", "Lefa",
+            ],
+            "reponse": [
+                "Kaaris", "Fianso",
+            ],
+        },
+    },
 ]
 
 const addQuestion = (question) => {
