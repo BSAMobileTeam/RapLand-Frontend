1f6f9c76ef87201eaf96f6d9c95dba9bc482c736
Froissart Kevin <kevin.froissart69@gmail.com>
2021-01-03 13:56:34 +0100

    Add appScreen component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Screen } from './app/component/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Screen style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </Screen>
   );
 }
 
