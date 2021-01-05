deb4a36a74abe13fea7ad13aced6858c904706cd
Matteo DE VITA <matteo.de-vita@epitech.eu>
2021-01-05 18:14:31 +0100

    add gameMode fake data and model

---
+++ data-temp/tempGameModes.ts
@@ -0,0 +1,23 @@
+import IGameMode from '../models/IGameMode'
+
+export default  [
+    {
+        name: 'rap us',
+        description: 'rap américain',
+        imageUrl: 'https://cdn.radiofrance.fr/s3/cruiser-production/2019/02/a22b7179-b241-4262-93fc-e821caaf5e5c/801x410_gettyimages-640370224.jpg'
+    },
+    {
+        name: 'rap fr',
+        description: 'rap francais',
+        imageUrl: 'https://intrld.com/wp-content/uploads/2019/11/meilleurs-demarrages-depuis-2013.jpg'
+    },
+    {
+        name: 'old rap',
+        description: 'en mode old school',
+        imageUrl: 'https://cdn.playlists.net/images/playlists/image/medium/252456-1438653553.jpg'
+    },
+    {
+        name: 'rap jeu',
+        imageUrl: 'https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2020/3/25/w78ndpjwlfkgfbgnvnme/mister-v-maskey-rap-jeu'
+    }
+] as IGameMode[] 
---
+++ models/IGameMode.ts
@@ -0,0 +1,5 @@
+export default interface IGameMode {
+    name: string,
+    description?: string,
+    imageUrl: string    
+}
