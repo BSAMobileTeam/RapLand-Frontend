acea378eba77bff0f67059501723105cac1d25f5
Matteo DE VITA <matteo.de-vita@epitech.eu>
2021-01-05 18:16:47 +0100

    add GameModePaper list in grid

--- app/components/GameModesDisplayer.tsx
+++ app/components/GameModesDisplayer.tsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
-
 import GameModePaper from './GameModePaper'
+import {
+    Col,
+    Row,
+    Grid
+} from "react-native-easy-grid"
+import IGameMode from '../../models/IGameMode'
+
+import tempGameModes from '../../data-temp/tempGameModes'
+
+import { 
+    StyleSheet,
+} from 'react-native';
 
 interface IGameModesDisplayerProps {
 
 }
 
 interface IGameModesDisplayerStates {
-
+    gameModes: IGameMode[][]
 }
+
 
 /**
  * Add grid to display several GameModePapers
@@ -18,18 +30,53 @@ export default class GameModesDisplayer extends Component<IGameModesDisplayerPro
     
     constructor (props: IGameModesDisplayerProps) {
         super(props)
+        this.getGameModes = this.getGameModes.bind(this)
         this.state = {
-
+            gameModes: this.getGameModes()
         }
     }
 
+    getGameModes(): IGameMode[][] {
+        //fetch data from server
+        let gameModes: IGameMode[] = tempGameModes
+        let splitedGameModes: IGameMode[][] = []
+        for (let i = 0; i < gameModes.length; i+= 3) {
+            splitedGameModes.push(gameModes.slice(i, i + 3))
+        }
+        // while (splitedGameModes[splitedGameModes.length - 1].length < 3) {
+        //     // splitedGameModes[splitedGameModes.length - 1].push(null)
+        // }            
+        return splitedGameModes
+    }
+
     render() {
+        const { gameModes } = this.state
         return (
-            <GameModePaper
-                name="RAP US"
-                backgroundUrl="https://cdn.radiofrance.fr/s3/cruiser-production/2019/02/a22b7179-b241-4262-93fc-e821caaf5e5c/801x410_gettyimages-640370224.jpg"
-                description="Quizz sur du rap us"
-            />
+            <Grid>
+                {
+                    gameModes.map((gameModesGroup: IGameMode[], index: number) => (
+                        <Row size={35} key={index}>
+                            {
+                                gameModesGroup.map((gameMode: IGameMode, index: number) => (
+                                    <Col style={styles.col} key={index}>
+                                        <GameModePaper
+                                            name={gameMode.name}
+                                            description={gameMode.description}
+                                            backgroundUrl={gameMode.imageUrl}
+                                        />
+                                    </Col>
+                                ))
+                            }
+                        </Row>
+                    ))
+                }
+            </Grid>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    col: {
+        height: 20
+    }
+})
